754ca39f87630c334d92bf487406934038bac1c0
AlexNykoliak <nykolyak.o@gmail.com>
2023-08-19 18:11:03 +0300

    Upd

---
+++ main.py
@@ -0,0 +1,102 @@
+# print("Hello Students")
+# print(10-20)
+# print(10*20)
+# print(10/20)  # 0.5
+# print(10//20)  # 0
+# print(5**5)
+# print(10+20)
+# print("10+20")
+
+# a = 10
+# b = "10"
+# c = 10.5
+# d = True
+# e = False
+# a = True
+
+
+# name = 'Roman'
+# last_name = 'Kachmar'
+# age = 29
+# print('Hello, my name is', name, last_name, 'I am', age, 'years old')
+# print(f'Hello, my name is {name} {last_name} I am {age} years old')
+# print('Hello, my name is {1} {2} I am {0} years old'.format(age, name, last_name ))
+
+
+# a = [1,2,3,4,5]
+# print(a[0])
+
+# a = 100
+
+# print(id(a) == id(100))
+
+# print(id(100)) 
+# print(id(a))  
+
+# a = 'elkfmeokfmefkmekfr'
+# print(hash(a))
+
+# Difference between hash and id
+
+
+
+
+
+# a = 10
+# b = 10
+
+
+# if a < b:
+#     print('a < b')
+# elif a > b:
+#     print('a > b')
+# else:
+#     print('a == b')
+
+# a = 1
+# b = '10'
+# print(a > len(b))
+
+
+# import random
+# import string
+
+# def password_generator(length):
+#     password = ''
+#     for i in range(length):
+#         password += random.choice(string.ascii_letters + string.digits + string.punctuation)
+#     print(password)
+    
+# password_generator(16)
+
+
+# a = -10
+# b = 10
+
+# if a < b:
+#     print('a < b')
+# elif a > b:
+#     print('a > b')
+# elif a < 0:
+#     print('a є від'ємною')
+  
+# print("Мар'яна") # 1
+# print('Мар"яна') # 2
+# print('Мар\'яна') # 3
+
+    
+# a = -10
+# b = 10
+
+# if a < b and a > 0:
+#     print('a < b and a > 0')
+# elif a > b:
+#     print('a > b')
+# elif a < 0:
+#     print('a є від\'ємною')
+    
+# a = 1
+# b = 4
+
+# if a > 0 or b < 3:
+#     print('a > 0 and b < 3')
